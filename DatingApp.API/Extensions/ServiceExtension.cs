c2923948ff5662628ee0b489cbfb43d63beeb9f6
edobasky <edore7@gmail.com>
2025-04-06 04:27:05 +0100

    added extension methods

---
+++ DatingApp.API/Extensions/ServiceExtension.cs
@@ -0,0 +1,26 @@
+﻿using DatingApp.API.Data;
+using DatingApp.API.Service.Interface;
+using DatingApp.API.Service;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Extensions
+{
+    public static class ServiceExtension
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services,IConfiguration config)
+        {
+            services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+            services.AddDbContext<DataContext>(opt =>
+            {
+                opt.UseSqlite(config.GetConnectionString("DefaultConn"));
+            });
+            services.AddCors();
+            services.AddScoped<ITokenService, TokenServiceImp>();
+
+            return services;
+        }
+    }
+}
--- DatingApp.API/Program.cs
+++ DatingApp.API/Program.cs
@@ -1,38 +1,11 @@
-using System.Text;
-using DatingApp.API.Data;
-using DatingApp.API.Service;
-using DatingApp.API.Service.Interface;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
+using DatingApp.API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>(opt =>
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConn"));
-});
-builder.Services.AddCors();
-builder.Services.AddScoped<ITokenService,TokenServiceImp>();
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        var tokenKey = builder.Configuration["TokenKey"];
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenKey!)),
-            ValidateIssuer = false,
-            ValidateAudience = false,
-            ClockSkew = TimeSpan.Zero,
-        };
-    });
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
 
