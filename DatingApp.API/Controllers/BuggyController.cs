c6a5cc6951f014745d56b0e36d1bae454edda678
edobasky <edore7@gmail.com>
2025-04-11 20:10:05 +0100

    implemented global error handler

---
+++ DatingApp.API/Controllers/BuggyController.cs
@@ -0,0 +1,52 @@
+﻿using DatingApp.API.Data;
+using DatingApp.API.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatingApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+   
+    public class BuggyController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+
+        public BuggyController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        [Authorize]
+        [HttpGet("auth")]
+        public ActionResult<string> GetAuth()
+        {
+            return "secret text";
+        }
+
+        [HttpGet("not-found")]
+        public ActionResult<AppUser> GetNotFound()
+        {
+            var thing = _dataContext.AppUsers.Find(-1);
+            if (thing == null) return NotFound();
+
+            return thing;
+        }
+
+        [HttpGet("server-error")]
+        public ActionResult<AppUser> GetServerError()
+        {
+            var thing = _dataContext.AppUsers.Find(-1) ?? throw new Exception("A bad thing happened");
+            return thing;
+
+
+        }
+
+        [HttpGet("bad-request")]
+        public ActionResult<string> GetBadRequest()
+        {
+            return BadRequest();
+        }
+    }
+}
--- DatingApp.API/Program.cs
+++ DatingApp.API/Program.cs
@@ -1,4 +1,5 @@
 using DatingApp.API.Extensions;
+using DatingApp.API.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,6 +16,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseMiddleware<ExceptionMiddleware>();
 
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
